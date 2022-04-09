5658d9cf7327e64c8f67032b4522edc458be127f
Rodrigo Zaninelli <rhzaninelli@gmail.com>
2022-04-09 18:09:05 -0300

    corrections and ToDos done.

--- src/application/Teste.java
+++ src/application/Teste.java
@@ -2,7 +2,6 @@ package application;
 
 import entities.Carga;
 import entities.Passeio;
-import entities.Veiculo;
 import utils.Leitura;
 
 import java.util.ArrayList;
@@ -35,7 +34,7 @@ public class Teste {
                 case 1:
                     String continua;
                     do{
-                        System.out.println("\n\n==================================");
+                        System.out.println("\n\n==================================\n");
                         String placa = lt.entDados("Entre com o valor da placa: ");
                         String marca = lt.entDados("Entre com o nome da marca: ");
                         String modelo = lt.entDados("Entre com o nome do modelo: ");
@@ -45,35 +44,31 @@ public class Teste {
                         int qtdPist = Integer.parseInt(lt.entDados("Entre com a quantidade de pistões do motor do veiculo: "));
                         int potencia = Integer.parseInt(lt.entDados("Entre com a potencia do veiculo: "));
                         int qtdPassageiros = Integer.parseInt(lt.entDados("Entre com a quantidade de passageiros do veiculo: "));
-
                         
-                        // TODO: 09/04/2022 - refatorar codigo abaixo -> não é possível sair de um switch case exceto com goto (não é bom utilizar goto)
+                        boolean existe = false;
                         for (Passeio veicPasseio: listPasseios) {
                             if (placa.equals(veicPasseio.getPlaca())){
-                                System.out.println("Jã existe um veiculo informado com a mesma placa!");
-                                lt.entDados("Aperte enter para continuar:");
-                                clearConsole();
-                                System.out.println("Entre com uma das opções abaixo:");
+                                existe = true;
                                 break;
                             }
                         }
+                        if (existe){
+                            System.out.println("\nJã existe um veiculo informado com a mesma placa!");
+                        }else{
+                            Passeio passeio = new Passeio(placa, marca, modelo, cor, velocMax, qtdRodas, qtdPist, potencia, qtdPassageiros);
+                            listPasseios.add(passeio);
+                            System.out.println("\nVeiculo de passeio placa " + placa + " adicionado!");
+                        }
 
-                        Passeio passeio = new Passeio(placa, marca, modelo, cor, velocMax, qtdRodas, qtdPist, potencia, qtdPassageiros);
-
-                        listPasseios.add(passeio);
-
-                        System.out.println("Veiculo de passeio placa " + placa + " adicionado!");
                         continua = lt.entDados("\nDeseja adicionar outro veiculo de passeio? (SIM ou NAO)");
                     }while(continua.equals("SIM"));
 
                     break;
 
                 case 2:
-                    
-                    // TODO: 09/04/2022 -> após resolver o problema do case 1, refatorar o case abaixo utilizando o mesmo formato do case acima
 
                     do{
-                        System.out.println("\n\n==================================");
+                        System.out.println("\n\n==================================\n");
                         String placa = lt.entDados("Entre com o valor da placa do veiculo: ");
                         String marca = lt.entDados("Entre com o nome da marca do veiculo: ");
                         String modelo = lt.entDados("Entre com o modelo do veiculo: ");
@@ -85,47 +80,48 @@ public class Teste {
                         int cargaMax = Integer.parseInt(lt.entDados("Entre com a carga maxima suportada pelo veiculo: "));
                         int tara = Integer.parseInt((lt.entDados("Entre com o peso de tara do veiculo: ")));
 
+                        boolean existe = false;
+
                         for (Carga veicCarga: listCarga) {
                             if (placa.equals(veicCarga.getPlaca())){
-                                System.out.println("Jã existe um veiculo informado com a mesma placa!");
-                                lt.entDados("Aperte enter para continuar:");
-                                clearConsole();
-                                System.out.println("Entre com uma das opções abaixo:");
+                                existe = true;
                                 break;
                             }
                         }
+                        if (existe){
+                            System.out.println("\nJã existe um veiculo de carga  informado com a mesma placa!");
+                        }else{
+                            Carga carga = new Carga(placa, marca, modelo, cor, velocMax, qtdRodas, qtdPist, potencia, cargaMax, tara);
+                            listCarga.add(carga);
+                            System.out.println("\nVeiculo de carga placa " + placa + " adicionado!");
+                        }
 
-                        Carga carga = new Carga(placa, marca, modelo, cor, velocMax, qtdRodas, qtdPist, potencia, cargaMax, tara);
-
-                        listCarga.add(carga);
-
-                        System.out.println("Veiculo de carga placa " + placa + " adicionado!");
                         continua = lt.entDados("\nDeseja adicionar outro veiculo de carga? (SIM ou NAO)");
-                        clearConsole();
+
                     }while (continua.equals("SIM"));
 
                     break;
 
                 case 3:
 
-                    System.out.println("\n\n==================================");
+                    System.out.println("\n\n==================================\n");
                     System.out.println("Os veiculos de passeio cadastrados são:");
                     for (Passeio veicPasseio: listPasseios) {
                         System.out.println(veicPasseio);
                     }
-                    System.out.println("Fim da lista de veiculos==========");
+                    System.out.println("\nFim da lista de veiculos==========");
                     lt.entDados("Tecle enter para retornar ao menu inicial:");
 
                     break;
 
                 case 4:
 
-                    System.out.println("\n\n==================================");
+                    System.out.println("\n\n==================================\n");
                     System.out.println("Os veiculos de carga cadastrados são:");
                     for (Carga veicCarga: listCarga) {
                         System.out.println(veicCarga    );
                     }
-                    System.out.println("Fim da lista de veiculos==========");
+                    System.out.println("\nFim da lista de veiculos==========");
                     lt.entDados("Tecle enter para retornar ao menu inicial:");
 
                     break;
@@ -136,13 +132,13 @@ public class Teste {
                     String placaPasseio = lt.entDados("Entre com a placa do veiculo de passeio: ");
                     for (Passeio veicPasseio: listPasseios) {
                         if (placaPasseio.equals(veicPasseio.getPlaca())){
-                            System.out.println("Seguem os dados do veiculo:");
+                            System.out.println("\nSeguem os dados do veiculo:");
                             System.out.printf("%s", veicPasseio.getPlaca());
-                            System.out.printf("Veiculo Passeio{placa=%s, marca=%s, modelo=%s, cor=%s, velocMax=%.2f km/h, qtdRodas=%d, motor=Motor{qtdPist= %d, potencia= d%c cv}, qtdPassageiros= %d, velocidade = %.2f}",
+                            System.out.printf("Veiculo Passeio{placa=%s, marca=%s, modelo=%s, cor=%s, velocMax=%.2f km/h, qtdRodas=%d, motor=Motor{qtdPist= %d, potencia= d%c cv}, qtdPassageiros= %d, velocidade = %.2f m/h}",
                                     veicPasseio.getPlaca(), veicPasseio.getMarca(), veicPasseio.getModelo(), veicPasseio.getCor(), veicPasseio.getVelocMax(), veicPasseio.getQtdRodas(), veicPasseio.getMotor().getQtdPist(), veicPasseio.getMotor().getPotencia(), veicPasseio.getQtdPassegeiros(), veicPasseio.calcVel(veicPasseio.getVelocMax()));
                             }
                     }
-                    lt.entDados("\nTecle enter para retornar ao menu inicial:");
+                    lt.entDados("\n\nTecle enter para retornar ao menu inicial:");
 
                     break;
 
@@ -152,11 +148,12 @@ public class Teste {
                     String placaCarga = lt.entDados("Entre com a placa do veiculo de carga: ");
                     for (Carga veicCarga: listCarga) {
                         if (placaCarga.equals(veicCarga.getPlaca())){
-                            System.out.println("Seguem os dados do veiculo:");
-                            System.out.println(veicCarga);
+                            System.out.println("\nSeguem os dados do veiculo:");
+                            System.out.printf("Veiculo Carga{placa=%s, marca=%s, modelo=%s, cor=%s, velocMax=%.2f km/h, qtdRodas=%d, motor=Motor{qtdPist= %d, potencia= d%c cv}, cargaMax= %d, tara= %d, velocidade = %.2f cm/h}",
+                                    veicCarga.getPlaca(), veicCarga.getMarca(), veicCarga.getModelo(), veicCarga.getCor(), veicCarga.getVelocMax(), veicCarga.getQtdRodas(), veicCarga.getMotor().getQtdPist(), veicCarga.getMotor().getPotencia(), veicCarga.getCargaMax(), veicCarga.getTara(), veicCarga.calcVel(veicCarga.getVelocMax()));
                         }
                     }
-                    lt.entDados("Tecle enter para retornar ao menu inicial:");
+                    lt.entDados("\n\nTecle enter para retornar ao menu inicial:");
 
                     break;
 
@@ -172,27 +169,6 @@ public class Teste {
 
                     break;
             }
-        }
-    }
-
-
-    // TODO: 09/04/2022  -> verificar funcionamento de clearConsole, codigo abaixo não funcionando como desejado. 
-    public static void clearConsole(){
-        try{
-            String operatingSystem = System.getProperty("os.name"); //Check the current operating system
-
-            if(operatingSystem.contains("Windows")){
-                ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "cls");
-                Process startProcess = pb.inheritIO().start();
-                startProcess.waitFor();
-            } else {
-                ProcessBuilder pb = new ProcessBuilder("clear");
-                Process startProcess = pb.inheritIO().start();
-
-                startProcess.waitFor();
-            }
-        }catch(Exception e){
-            System.out.println(e);
         }
     }
 }
